50bfe72b727c516b3aee8c251db48b3d6429e0e9
Auryn Macmillan <hi@auryn.xyz>
2021-08-29 12:17:21 -0400

    Update coverage

--- .solcover.js
+++ .solcover.js
@@ -1,9 +1,5 @@
 module.exports = {
-  skipFiles: [
-    "test/TestAvatar.sol",
-    "test/TestModule.sol",
-    "test/TestModifier.sol",
-  ],
+  skipFiles: ["test"],
   mocha: {
     grep: "@skip-on-coverage", // Find everything with this tag
     invert: true, // Run the grep's inverse set.
