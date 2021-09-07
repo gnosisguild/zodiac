67a0956e2bce11b5945cc79f1aff4ee3a0a4ea2a
Auryn Macmillan <hi@auryn.xyz>
2021-09-07 13:18:44 -0400

    Add target to Module.sol

--- contracts/test/TestModifier.sol
+++ contracts/test/TestModifier.sol
@@ -23,8 +23,8 @@ contract TestModifier is Modifier {
         bool success
     );
 
-    constructor(address _avatar) {
-        bytes memory initParams = abi.encode(_avatar);
+    constructor(address _avatar, address _target) {
+        bytes memory initParams = abi.encode(_avatar, _target);
         setUp(initParams);
     }
 
@@ -74,8 +74,12 @@ contract TestModifier is Modifier {
 
     function setUp(bytes memory initializeParams) public override {
         __Ownable_init();
-        address _avatar = abi.decode(initializeParams, (address));
+        (address _avatar, address _target) = abi.decode(
+            initializeParams,
+            (address, address)
+        );
         avatar = _avatar;
+        target = _target;
         initialized = true;
     }
 }
