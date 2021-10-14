193e6a88f987046073842d331d6928ba96bce744
byterose <byterose@tutanota.com>
2021-10-14 16:26:40 +0200

    Add custom errors

--- package/contracts/guard/Guardable.sol
+++ package/contracts/guard/Guardable.sol
@@ -3,23 +3,22 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@gnosis.pm/safe-contracts/contracts/interfaces/IERC165.sol";
 import "./BaseGuard.sol";
 
 /// @title Guardable - A contract that manages fallback calls made to this contract
 contract Guardable is OwnableUpgradeable {
+    address public guard;
+
     event ChangedGuard(address guard);
 
-    address public guard;
+    /// @notice Guard does not implement IERC165.
+    error NotIERC165Compliant();
 
     /// @dev Set a guard that checks transactions before execution
     /// @param _guard The address of the guard to be used or the 0 address to disable the guard
     function setGuard(address _guard) external onlyOwner {
         if (_guard != address(0)) {
-            require(
-                BaseGuard(_guard).supportsInterface(type(IGuard).interfaceId),
-                "Guard does not implement IERC165"
-            );
+            if (!BaseGuard(_guard).supportsInterface(type(IGuard).interfaceId)) revert NotIERC165Compliant(); 
         }
         guard = _guard;
         emit ChangedGuard(guard);
