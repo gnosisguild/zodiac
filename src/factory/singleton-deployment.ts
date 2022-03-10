409a3dc420d889f9234cfd5cce73d94c4fd8079d
cbrzn <cesarbrazon10@gmail.com>
2022-03-10 21:16:22 +0100

    factory: update readme and fix build

--- src/factory/singleton-deployment.ts
+++ src/factory/singleton-deployment.ts
@@ -3,17 +3,17 @@ import "@nomiclabs/hardhat-ethers";
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-const { bytecode: factoryDeploymentBytecode } = require("../../build/artifacts/contracts/factory/ModuleProxyFactory.sol/ModuleProxyFactory.json")
 const singletonFactoryAbi = [ 'function deploy(bytes memory _initCode, bytes32 _salt) public returns (address payable createdContract)']
 const singletonFactoryAddress = '0xce0042b868300000d44a59004da54a005ffdcf9f'
 const factorySalt = '0xddab1fa8c9085d8d8677711bcd78e94f7af3cbd1ca711def31ea3e0cf982d90c'
 
 const deployFactory = async (_: null, hardhat: HardhatRuntimeEnvironment) => {
+    const factory = await hardhat.ethers.getContractFactory("ModuleProxyFactory")
     const singletonFactory = new hardhat.ethers.Contract(singletonFactoryAddress, singletonFactoryAbi)
     const encodedData = singletonFactory.interface.encodeFunctionData(
         'deploy(bytes memory _initCode, bytes32 _salt)',
         [
-            factoryDeploymentBytecode,
+            factory.bytecode,
             factorySalt
         ]
     )
