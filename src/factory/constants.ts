b8654361289fa81103ef1f5b2be3946c7a37d638
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-11-16 15:33:16 +0100

    add all ABIs as json

--- src/factory/constants.ts
+++ src/factory/constants.ts
@@ -1,6 +1,22 @@
-import Erc20VotesAbi from "../abi/erc20_votes";
-import Erc721VotesAbi from "../abi/erc721_votes";
-import OzGovernorAbi from "../abi/oz_governor";
+import BridgeAbi from "../abi/Bridge.json";
+import CirculatingSupplyErc20Abi from "../abi/CirculatingSupplyErc20.json";
+import CirculatingSupplyErc721Abi from "../abi/CirculatingSupplyErc721.json";
+import DelayAbi from "../abi/Delay.json";
+import Erc20VotesAbi from "../abi/Erc20Votes.json";
+import Erc721VotesAbi from "../abi/Erc721Votes.json";
+import ExitErc20Abi from "../abi/ExitErc20.json";
+import ExitErc721Abi from "../abi/ExitErc721.json";
+import MetaGuardAbi from "../abi/MetaGuard.json";
+import ModuleProxyFactoryAbi from "../abi/ModuleProxyFactory.json";
+import OptimisticGovernorAbi from "../abi/OptimisticGovernor.json";
+import OzGovernorAbi from "../abi/OzGovernor.json";
+// import PermissionsAbi from "../abi/Permissions.json";
+import RealityErc20Abi from "../abi/RealityErc20.json";
+import RealityEthAbi from "../abi/RealityEth.json";
+import RolesAbi from "../abi/Roles.json";
+import ScopeGuardAbi from "../abi/ScopeGuard.json";
+import TellorAbi from "../abi/Tellor.json";
+// import UsulAbi from "../abi/Usul.json";
 import { KnownContracts } from "./types";
 
 export enum SUPPORTED_NETWORKS {
@@ -75,192 +91,23 @@ export const CONTRACT_ADDRESSES: Record<
 };
 
 export const CONTRACT_ABIS: Record<KnownContracts, any> = {
-  [KnownContracts.META_GUARD]: [
-    `function setUp(bytes memory initParams) public`,
-    `function setAvatar(address _avatar) public`,
-    `function setMaxGuard(uint256 _maxGuard) public`,
-    `function checkTransaction(
-      address to,
-      uint256 value,
-      bytes memory data,
-      Enum.Operation operation,
-      uint256 safeTxGas,
-      uint256 baseGas,
-      uint256 gasPrice,
-      address gasToken,
-      address payable refundReceiver,
-      bytes memory signatures,
-      address msgSender
-    ) external`,
-    `function checkAfterExecution(bytes32 txHash, bool success) external`,
-    `function removeGuard(address prevGuard, address guard) public`,
-    `function addGuard(address guard) public`,
-    `function isGuardAdded(address _guard) public`,
-    `function getAllGuards() external`,
-  ],
-  [KnownContracts.OPTIMISTIC_GOVERNOR]: [
-    `function setUp(bytes memory initParams) public`,
-  ],
-  [KnownContracts.TELLOR]: [`function setUp(bytes memory initParams) public`],
-  [KnownContracts.REALITY_ETH]: [
-    `function setArbitrator(address arbitrator) public`,
-    `function setQuestionTimeout(uint32 timeout) public`,
-    `function setQuestionCooldown(uint32 cooldown) public`,
-    `function setMinimumBond(uint256 bond) public`,
-    `function setTemplate(bytes32 template) public`,
-    `function setAnswerExpiration(uint32 expiration) public`,
-    `function setUp(bytes memory initParams) public`,
-  ],
-  [KnownContracts.REALITY_ERC20]: [
-    `function setArbitrator(address arbitrator) public`,
-    `function setQuestionTimeout(uint32 timeout) public`,
-    `function setQuestionCooldown(uint32 cooldown) public`,
-    `function setMinimumBond(uint256 bond) public`,
-    `function setTemplate(bytes32 template) public`,
-    `function setAnswerExpiration(uint32 expiration) public`,
-    `function setUp(bytes memory initParams) public`,
-  ],
-  [KnownContracts.BRIDGE]: [
-    `function setAmb(address _amb) public`,
-    `function setChainId(bytes32 _chainId) public`,
-    `function setOwner(address _owner) public`,
-    `function setUp(bytes memory initParams) public`,
-  ],
-  [KnownContracts.DELAY]: [
-    `function setTxCooldown(uint256 cooldown) public`,
-    `function setTxExpiration(uint256 expiration) public`,
-    `function setUp(bytes memory initParams) public`,
-    `function enableModule(address module) public`,
-    `function txCooldown() public view returns (uint256)`,
-    `function txExpiration() public view returns (uint256)`,
-    `function getModulesPaginated(
-      address start,
-      uint256 pageSize
-    ) external view returns (
-      address[] memory array,
-      address next
-    )`,
-  ],
-  [KnownContracts.EXIT_ERC20]: [
-    `function setUp(bytes memory initParams) public`,
-    `function exit(uint256 amountToRedeem, address[] calldata tokens) public`,
-    `function addToDenylist(address[] calldata tokens) external`,
-    `function removeFromDenylist(address[] calldata tokens) external `,
-    `function setDesignatedToken(address _token) public`,
-    `function getCirculatingSupply() public view returns (uint256)`,
-  ],
-  [KnownContracts.EXIT_ERC721]: [
-    `function setUp(bytes memory initParams) public`,
-    `function exit(uint256 tokenId, address[] calldata tokens) public`,
-    `function addToDenylist(address[] calldata tokens) external`,
-    `function removeFromDenylist(address[] calldata tokens) external `,
-    `function setDesignatedToken(address _token) public`,
-    `function getCirculatingSupply() public view returns (uint256)`,
-  ],
-  [KnownContracts.SCOPE_GUARD]: [
-    `function setUp(bytes memory initParams) public`,
-    `function checkTransaction(
-      address to,
-      uint256,
-      bytes memory data,
-      uint8 operation,
-      uint256,
-      uint256,
-      uint256,
-      address,
-      address payable,
-      bytes memory,
-      address
-    ) external view`,
-    `function isAllowedToDelegateCall(address target) public view returns (bool)`,
-    `function isAllowedFunction(address target, bytes4 functionSig) public view returns (bool)`,
-    `function isScoped(address target) public view returns (bool)`,
-    `function isAllowedTarget(address target) public view returns (bool)`,
-    `function disallowFunction(address target, bytes4 functionSig) public returns (bool)`,
-    `function allowFunction(address target, bytes4 functionSig) public`,
-    `function toggleScoped(address target) public`,
-    `function disallowDelegateCall(address target) public`,
-    `function allowDelegateCall(address target) public`,
-    `function disallowTarget(address target) public`,
-    `function allowTarget(address target) public`,
-  ],
-  [KnownContracts.FACTORY]: [
-    `function deployModule(
-      address masterCopy,
-      bytes memory initializer,
-      uint256 saltNonce
-    ) public returns (address proxy)`,
-  ],
-  [KnownContracts.CIRCULATING_SUPPLY_ERC20]: [
-    `function setUp(bytes memory initializeParams) public`,
-    `function get() public view returns (uint256 circulatingSupply)`,
-    `function setToken(address _token) public`,
-    `function removeExclusion(address prevExclusion, address exclusion) public`,
-    `function exclude(address exclusion) public`,
-    `function isExcluded(address _exclusion) public view returns (bool)`,
-    `function getExclusionsPaginated(address start, uint256 pageSize) public view`,
-  ],
-  [KnownContracts.CIRCULATING_SUPPLY_ERC721]: [
-    `function setUp(bytes memory initializeParams) public`,
-    `function get() public view returns (uint256 circulatingSupply)`,
-    `function setToken(address _token) public`,
-    `function removeExclusion(address prevExclusion, address exclusion) public`,
-    `function exclude(address exclusion) public`,
-    `function isExcluded(address _exclusion) public view returns (bool)`,
-    `function getExclusionsPaginated(address start, uint256 pageSize) public view`,
-  ],
-  [KnownContracts.ROLES]: [
-    "constructor(address _owner, address _avatar, address _target)",
-    "error ArraysDifferentLength()",
-    "error ModuleTransactionFailed()",
-    "error NoMembership()",
-    "error SetUpModulesAlreadyCalled()",
-    "event AssignRoles(address module, uint16[] roles, bool[] memberOf)",
-    "event AvatarSet(address indexed previousAvatar, address indexed newAvatar)",
-    "event ChangedGuard(address guard)",
-    "event DisabledModule(address module)",
-    "event EnabledModule(address module)",
-    "event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)",
-    "event RolesModSetup(address indexed initiator, address indexed owner, address indexed avatar, address target)",
-    "event SetDefaultRole(address module, uint16 defaultRole)",
-    "event SetMultisendAddress(address multisendAddress)",
-    "event TargetSet(address indexed previousTarget, address indexed newTarget)",
-    "function allowTarget(uint16 role, address targetAddress, uint8 options)",
-    "function assignRoles(address module, uint16[] _roles, bool[] memberOf)",
-    "function avatar() view returns (address)",
-    "function defaultRoles(address) view returns (uint16)",
-    "function disableModule(address prevModule, address module)",
-    "function enableModule(address module)",
-    "function execTransactionFromModule(address to, uint256 value, bytes data, uint8 operation) returns (bool success)",
-    "function execTransactionFromModuleReturnData(address to, uint256 value, bytes data, uint8 operation) returns (bool, bytes)",
-    "function execTransactionWithRole(address to, uint256 value, bytes data, uint8 operation, uint16 role, bool shouldRevert) returns (bool success)",
-    "function execTransactionWithRoleReturnData(address to, uint256 value, bytes data, uint8 operation, uint16 role, bool shouldRevert) returns (bool success, bytes returnData)",
-    "function getGuard() view returns (address _guard)",
-    "function getModulesPaginated(address start, uint256 pageSize) view returns (address[] array, address next)",
-    "function guard() view returns (address)",
-    "function isModuleEnabled(address _module) view returns (bool)",
-    "function multisend() view returns (address)",
-    "function owner() view returns (address)",
-    "function renounceOwnership()",
-    "function revokeTarget(uint16 role, address targetAddress)",
-    "function scopeAllowFunction(uint16 role, address targetAddress, bytes4 functionSig, uint8 options)",
-    "function scopeFunction(uint16 role, address targetAddress, bytes4 functionSig, bool[] isParamScoped, uint8[] paramType, uint8[] paramComp, bytes[] compValue, uint8 options)",
-    "function scopeFunctionExecutionOptions(uint16 role, address targetAddress, bytes4 functionSig, uint8 options)",
-    "function scopeParameter(uint16 role, address targetAddress, bytes4 functionSig, uint256 paramIndex, uint8 paramType, uint8 paramComp, bytes compValue)",
-    "function scopeParameterAsOneOf(uint16 role, address targetAddress, bytes4 functionSig, uint256 paramIndex, uint8 paramType, bytes[] compValues)",
-    "function scopeRevokeFunction(uint16 role, address targetAddress, bytes4 functionSig)",
-    "function scopeTarget(uint16 role, address targetAddress)",
-    "function setAvatar(address _avatar)",
-    "function setDefaultRole(address module, uint16 role)",
-    "function setGuard(address _guard)",
-    "function setMultisend(address _multisend)",
-    "function setTarget(address _target)",
-    "function setUp(bytes initParams)",
-    "function target() view returns (address)",
-    "function transferOwnership(address newOwner)",
-    "function unscopeParameter(uint16 role, address targetAddress, bytes4 functionSig, uint8 paramIndex)",
-  ],
-  ozGovernor: OzGovernorAbi,
-  erc20Votes: Erc20VotesAbi,
-  erc721Votes: Erc721VotesAbi,
+  [KnownContracts.BRIDGE]: BridgeAbi,
+  [KnownContracts.CIRCULATING_SUPPLY_ERC20]: CirculatingSupplyErc20Abi,
+  [KnownContracts.CIRCULATING_SUPPLY_ERC721]: CirculatingSupplyErc721Abi,
+  [KnownContracts.DELAY]: DelayAbi,
+  [KnownContracts.ERC20_VOTES]: Erc20VotesAbi,
+  [KnownContracts.ERC721_VOTES]: Erc721VotesAbi,
+  [KnownContracts.EXIT_ERC20]: ExitErc20Abi,
+  [KnownContracts.EXIT_ERC721]: ExitErc721Abi,
+  [KnownContracts.FACTORY]: ModuleProxyFactoryAbi,
+  [KnownContracts.META_GUARD]: MetaGuardAbi,
+  [KnownContracts.OPTIMISTIC_GOVERNOR]: OptimisticGovernorAbi,
+  [KnownContracts.OZ_GOVERNOR]: OzGovernorAbi,
+  // [KnownContracts.PERMISSIONS]: PermissionsAbi,
+  [KnownContracts.REALITY_ERC20]: RealityErc20Abi,
+  [KnownContracts.REALITY_ETH]: RealityEthAbi,
+  [KnownContracts.ROLES]: RolesAbi,
+  [KnownContracts.SCOPE_GUARD]: ScopeGuardAbi,
+  [KnownContracts.TELLOR]: TellorAbi,
+  // [KnownContracts.USUL]: UsulAbi,
 };
