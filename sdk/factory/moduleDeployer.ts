0b04d6c1b73ee42f7f745ef490b07cfbe61fb9a9
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-09-29 12:30:32 +0200

    format

--- sdk/factory/moduleDeployer.ts
+++ sdk/factory/moduleDeployer.ts
@@ -1,5 +1,6 @@
 import { Provider } from "@ethersproject/providers";
 import { ethers, Contract, Signer, BigNumber } from "ethers";
+import { KnownContracts } from "./types";
 import {
   ContractAddresses,
   ContractAbis,
@@ -7,8 +8,6 @@ import {
   ContractFactories,
 } from "../contracts";
 import { ModuleProxyFactory__factory } from "../types";
-
-import { KnownContracts } from "./types";
 
 type ABI = any[] | readonly any[];
 
