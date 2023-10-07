06c4a27a2a98bb2d420b01cd3f4337255e4c1711
cristovaoth <cristovao.honorato@gmail.com>
2023-10-07 16:47:47 +0200

    remove import

--- contracts/core/Modifier.sol
+++ contracts/core/Modifier.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.7.0 <0.9.0;
 
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-
 import "../interfaces/IAvatar.sol";
 import "../signature/SignatureChecker.sol";
 import "./Module.sol";
