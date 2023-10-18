41f16eb95748f31d8529ffcd3e37b6dc348191d0
Cristovao Honorato <cristovao.honorato@gmail.com>
2023-10-18 09:46:09 +0200

    Consolidate hash tracking into one mapping

--- contracts/signature/ExecutionTracker.sol
+++ contracts/signature/ExecutionTracker.sol
@@ -3,13 +3,15 @@ pragma solidity >=0.8.0 <0.9.0;
 
 /// @title ExecutionTracker - A contract that keeps track of executed and invalidated hashes
 contract ExecutionTracker {
-    error HashAlreadyExecuted(bytes32);
-    error HashInvalidated(bytes32);
+    error HashAlreadyConsumed(bytes32);
 
-    mapping(address => mapping(bytes32 => bool)) public executed;
-    mapping(address => mapping(bytes32 => bool)) public invalidated;
+    event HashExecuted(bytes32);
+    event HashInvalidated(bytes32);
+
+    mapping(address => mapping(bytes32 => bool)) public consumed;
 
     function invalidate(bytes32 hash) external {
-        invalidated[msg.sender][hash] = true;
+        consumed[msg.sender][hash] = true;
+        emit HashInvalidated(hash);
     }
 }
